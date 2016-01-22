4e4283d0a612907f6489c2172d44eb50113a8fb2
Fred <fredchenca@live.com>
2016-01-21 19:16:12 -0500

    delete template

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require('head.php'); ?>
+</head>
+
+<body>
+    <?php require('menu.php'); ?>
+    
+    <div id="content_div"></div>
+
+    <?php include('footer.php'); ?>
+
+</body>
+</html>
